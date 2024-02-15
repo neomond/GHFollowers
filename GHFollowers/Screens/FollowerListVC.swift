6392e16ef74604531c79859319e19c3e93a8a554
neomond <neoweise@yahoo.com>
2024-02-16 01:58:02 +0400

    fix: Refactor Error Message

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -15,11 +15,20 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { ( followers, errorMessage)  in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue , buttonTitle: "Ok")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     override func  viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true) 
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
   
 
---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Nazrin Atayeva on 15.02.24.
+//
+
+import Foundation
+
+
+enum ErrorMessage: String {
+    case invalidUsername    = "This username create an invalid request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid. Please try again."
+}
