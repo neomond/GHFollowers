9c1ecde0cd26267d08c51d5f05562c2d379e5982
neomond <neoweise@yahoo.com>
2024-02-17 13:45:08 +0400

    Download & Cache Avatar Images

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Nazrin Atayeva on 15.02.24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared   = NetworkManager()
-    let baseURL         = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache           = NSCache<NSString, UIImage>()
     
     private init(){}
     
