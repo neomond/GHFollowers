42461e4d684bafe9a73fdc5dfdfc60eabaf55182
neomond <neoweise@yahoo.com>
2024-02-21 10:15:18 +0400

    feat: UserInfoVC Cleanup

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -9,16 +9,59 @@ import UIKit
 
 class UserInfoVC: UIViewController {
 
-    let headerView = UIView()
+    let headerView              = UIView()
+    let itemViewOne             = UIView()
+    let itemViewTwo             = UIView()
+    var itemViews: [UIView]     = []
+    
     var username: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        configureViewController()
+        layoutUI()
+        getUserInfo()
+     
+    }
+    
+    
+    func layoutUI(){
+        let padding: CGFloat        = 20
+        let itemHeight: CGFloat     = 140
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+        
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            ])
+        }
+         
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+        ])
+    }
+    
+    
+    func configureViewController(){
         view.backgroundColor = .systemBackground
         let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneBtn
-        
-        layoutUI()
+    }
+    
+    func getUserInfo(){
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             
@@ -32,21 +75,6 @@ class UserInfoVC: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
-        print(username!)
-       
-    }
-    
-    
-    func layoutUI(){
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
-        ])
     }
     
     
